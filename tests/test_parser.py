220215efa3e8ebd931c663a19c0c254f00f2d86a
jackwardell <jack@wardell.xyz>
2020-12-14 18:44:19 +0000

    adding relative tag test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,12 +6,15 @@ HTML_NO_A_TAGS = "<html><body><h1>hello world</h1></body></html>"
 HTML_ONE_A_TAGS = "<html><body><a href='/hello-world'>hello world</a></body></html>"
 HTML_TWO_A_TAGS = "<html><body><a href='https://www.google.com'>google</a><a href='/hello-world'>hello world</a></body></html>"
 HTML_ABSOLUTE_A_TAG = "<html><body><a href='https://example.com'>hello world</a></body></html>"
+HTML_RELATIVE_A_TAG = "<html><body><a href='/example.html'>hello world</a></body></html>"
+
 
 HTML_SNIPPETS_AND_RESULT = [
     (HTML_NO_A_TAGS, set()),
     (HTML_ONE_A_TAGS, {"/hello-world"}),
     (HTML_TWO_A_TAGS, {"https://www.google.com", "/hello-world"}),
     (HTML_ABSOLUTE_A_TAG, {"https://example.com"}),
+    (HTML_RELATIVE_A_TAG, {"/example.html"}),
 ]
 
 
