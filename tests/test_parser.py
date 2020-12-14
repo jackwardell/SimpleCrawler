33a00286f855e4d564978bd334187ca8c19d283c
jackwardell <jack@wardell.xyz>
2020-12-14 19:01:02 +0000

    refactor: simpler design for link testing

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,24 +2,28 @@ import pytest
 
 from crawler.parser import get_links_from_html
 
-HTML_NO_A_TAGS = "<html><body><h1>hello world</h1></body></html>"
-HTML_ONE_A_TAGS = "<html><body><a href='/hello-world'>hello world</a></body></html>"
-HTML_TWO_A_TAGS = "<html><body><a href='https://www.google.com'>google</a><a href='/hello-world'>hello world</a></body></html>"
-HTML_ABSOLUTE_A_TAG = "<html><body><a href='https://example.com'>hello world</a></body></html>"
-HTML_RELATIVE_A_TAG = "<html><body><a href='/example.html'>hello world</a></body></html>"
-HTML_GET_ARGS_A_TAG = (
-    "<html><body><a href='/example.html?hello=world'>hello world</a></body></html>"
-)
-HTML_FRAGMENT_A_TAG = "<html><body><a href='#hello'>hello world</a></body></html>"
+
+def make_html(body: str) -> str:
+    return f"<html><head></head><body>{body}</body></html>"
+
+
+def make_a_tag(path: str) -> str:
+    return f'<a href="{path}">{path}</a>'
+
+
+def make_a_tags(paths: list) -> str:
+    return "<br>".join([make_a_tag(path) for path in paths])
+
+
+ABSOLUTE_LINK = ["https://example.com"]
+RELATIVE_LINK = ["/example.html"]
+GET_ARGS_LINK = ["/example?hello=world&world=hello"]
+FRAGMENT_LINK = ["#hello"]
+
+LINK_EXAMPLES = [ABSOLUTE_LINK, RELATIVE_LINK, GET_ARGS_LINK, FRAGMENT_LINK]
 
 HTML_SNIPPETS_AND_RESULT = [
-    (HTML_NO_A_TAGS, set()),
-    (HTML_ONE_A_TAGS, {"/hello-world"}),
-    (HTML_TWO_A_TAGS, {"https://www.google.com", "/hello-world"}),
-    (HTML_ABSOLUTE_A_TAG, {"https://example.com"}),
-    (HTML_RELATIVE_A_TAG, {"/example.html"}),
-    (HTML_GET_ARGS_A_TAG, {"/example.html?hello=world"}),
-    (HTML_FRAGMENT_A_TAG, {"#hello"}),
+    (make_html(make_a_tags(example)), set(example)) for example in LINK_EXAMPLES
 ]
 
 
