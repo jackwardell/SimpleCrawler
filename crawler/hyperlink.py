1cd2209c6afa18daedd98efb89e9cc3d812eb303
jackwardell <jack@wardell.xyz>
2020-12-24 10:11:59 +0000

    refactoring some bits after a while

--- crawler/hyperlink.py
+++ crawler/hyperlink.py
@@ -1,6 +1,12 @@
 import urllib.parse
+from typing import Union
 
+from crawler.url_normalisation import normalise_authority
+from crawler.url_normalisation import normalise_fragment
 from crawler.url_normalisation import normalise_kwargs
+from crawler.url_normalisation import normalise_path
+from crawler.url_normalisation import normalise_query
+from crawler.url_normalisation import normalise_scheme
 from crawler.url_normalisation import normalise_url
 
 
@@ -9,35 +15,51 @@ class Hyperlink:
     a representation of a Hyperlink REFerence (href)
     """
 
-    __slots__ = "url"
+    __slots__ = "url", "_input_url"
 
     def __init__(self, link: str):
         # split is the core element we want to build class around
-        self.url = link
+        self._input_url = link
+        self.url = normalise_url(link)
 
     @property
     def components(self) -> urllib.parse.SplitResult:
-        return urllib.parse.urlsplit(self.url)
+        return urllib.parse.urlsplit(self._input_url)
 
     @property
     def scheme(self) -> str:
-        return self.components.scheme
+        return normalise_scheme(self.components.scheme)
 
     @property
     def authority(self) -> str:
-        return self.components.netloc
+        return normalise_authority(self.components.netloc)
 
     @property
     def path(self) -> str:
-        return self.components.path
+        return normalise_path(self.components.path)
 
     @property
     def query(self) -> str:
-        return self.components.query
+        return normalise_query(self.components.query)
 
     @property
     def fragment(self) -> str:
-        return self.components.fragment
+        return normalise_fragment(self.components.fragment)
+
+    @property
+    def domain(self):
+        """this is the scheme and authority e.g. www.example.com"""
+        scheme, authority, *_ = self.components
+        return Hyperlink(urllib.parse.urlunsplit((scheme, authority, "", "", "")))
+
+    def without_fragment(self):
+        """remove fragment from href"""
+        scheme, authority, path, query, _ = self.components
+        return Hyperlink(urllib.parse.urlunsplit((scheme, authority, path, query, "")))
+
+    def with_path(self, path):
+        """join path to self as base url"""
+        return Hyperlink(self.domain.url + path)
 
     def __str__(self):
         return self.url
@@ -61,14 +83,15 @@ class Hyperlink:
         """all links that don't start with a scheme (e.g. https) are relative"""
         return not self.is_absolute
 
-    def join(self, host: str):
+    def join(self, base_url: Union[str, "Hyperlink"]):
         """
-        bind a href to a host if possible (e.g. /example -> https://www.example.com/example)
-        :param host: (str) an
-        :return:
+        bind a href to a base_url if possible (e.g. /example -> https://www.example.com/example)
+
+        :param base_url: (str) an base_url like https://www.example.com
+        :return: Hyperlink bound to an base_url
         """
-        host = normalise_url(host)
-        resolution = urllib.parse.urljoin(host, self.url)
+        base_url = make_hyperlink(base_url)
+        resolution = urllib.parse.urljoin(base_url._input_url, self._input_url)
         return make_hyperlink(resolution)
 
     @classmethod
@@ -79,11 +102,13 @@ class Hyperlink:
         :param link: (str) any link/uri
         :return: (Hyperlink) an instance of hyperlink
         """
+        if isinstance(link, cls):
+            return link
+
         if not isinstance(link, str):
             raise TypeError("href links need to be strings")
 
-        normalised_link = normalise_url(link)
-        return cls(normalised_link)
+        return cls(link)
 
 
 make_hyperlink = Hyperlink.make
@@ -133,14 +158,17 @@ class HyperlinkCollection:
         # quickest way to dedupe while retaining order
         return HyperlinkCollection(list(dict.fromkeys(self.collection)))
 
-    def join_all(self, host: str):
+    def join_all(self, base_url: Union[str, Hyperlink]):
         """
         apply join to all items in the collection and return a collection with those values
 
-        :param host: (str) host to apply join to on HyperlinkReference
+        :param base_url: (str) host to apply join to on HyperlinkReference
         :return: new instance of HyperlinkReferenceCollection that has entries all joined
+
+        todo: base_url to Hyperlink
         """
-        return HyperlinkCollection([link.join(host) for link in self.collection])
+        base_url = make_hyperlink(base_url)
+        return HyperlinkCollection([link.join(base_url) for link in self.collection])
 
     def filter_by(self, **kwargs):
         """
@@ -157,6 +185,10 @@ class HyperlinkCollection:
             if all([getattr(link, k) == v for k, v in kwargs.items()]):
                 results.append(link)
         return HyperlinkCollection(results)
+
+    def without_fragments(self):
+        """apply without fragments to all hrefs"""
+        return HyperlinkCollection([href.without_fragment() for href in self.collection])
 
     @classmethod
     def make(cls, links: list = None):
