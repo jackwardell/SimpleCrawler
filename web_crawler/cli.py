0004213c397865f222fc62dc021134fde23b68eb
jackwardell <jack@wardell.xyz>
2020-12-24 12:11:05 +0000

    upgrading cli

--- web_crawler/cli.py
+++ web_crawler/cli.py
@@ -2,6 +2,8 @@ import click
 
 from web_crawler.crawler import Crawler
 from web_crawler.crawler import DEFAULT_CHECK_HEAD
+from web_crawler.crawler import DEFAULT_MAX_WORKERS
+from web_crawler.crawler import DEFAULT_OBEY_ROBOTS
 from web_crawler.crawler import DEFAULT_TIMEOUT
 from web_crawler.crawler import DEFAULT_USER_AGENT
 
@@ -9,19 +11,32 @@ from web_crawler.crawler import DEFAULT_USER_AGENT
 @click.command()
 @click.argument("url")
 @click.option("-u", "--user-agent", default=DEFAULT_USER_AGENT)
+@click.option("-w", "--max-workers", default=DEFAULT_MAX_WORKERS)
 @click.option("-t", "--timeout", default=DEFAULT_TIMEOUT)
 @click.option("-h", "--check-head", is_flag=True, default=DEFAULT_CHECK_HEAD)
+@click.option("-o", "--obey-robots", is_flag=True, default=DEFAULT_OBEY_ROBOTS)
 @click.option("--debug/--no-debug", default=False)
-def crawl(url, user_agent, timeout, check_head, debug):
+def crawl(url, user_agent, max_workers, timeout, check_head, obey_robots, debug):
     click.echo(f"Crawling URL: {url}")
-    crawler = Crawler(user_agent=user_agent, timeout=timeout, check_head=check_head)
+    crawler = Crawler(
+        user_agent=user_agent,
+        max_workers=max_workers,
+        timeout=timeout,
+        check_head=check_head,
+        obey_robots=obey_robots,
+    )
 
     if debug is False:
-        results = crawler.crawl(url)
-        click.echo(f"Found: {results}")
+        crawler.crawl(url)
 
     else:
         click.echo("Debug mode is on: crawling not running")
         click.echo(f"crawler User-Agent: {crawler.user_agent}")
+        click.echo(f"crawler max workers: {crawler.max_workers}")
         click.echo(f"crawler timeout: {crawler.timeout}")
         click.echo(f"crawler check head: {crawler.check_head}")
+        click.echo(f"crawler obey robots: {crawler.obey_robots}")
+
+
+if __name__ == "__main__":
+    crawl()
