7bacc64de6d0487f0078b3fed0718651c970e6bc
jackwardell <jack@wardell.xyz>
2020-12-24 10:15:45 +0000

    refactoring and making installable

---
+++ setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+from setuptools import find_packages
+from setuptools import setup
+
+__version__ = "0.1.0"
+ROOT_DIR = Path(".")
+
+with open(str(ROOT_DIR / "README.md")) as readme:
+    long_description = readme.read()
+
+setup(
+    name="WebCrawler",
+    version=__version__,
+    packages=find_packages(),
+    include_package_data=True,
+    author="jackwardell",
+    author_email="jack@wardell.xyz",
+    url="https://github.com/jackwardell",
+    description="Enter something here",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    test_suite="tests",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Intended Audience :: Developers",
+    ],
+    keywords="python",
+    python_requires=">=3.5",
+)
