0b7f1bc3a0484568deb56bdeaadde903ccedbd9c
jackwardell <jack@wardell.xyz>
2020-12-30 10:39:21 +0000

    refactoring crawler to use new hyperlink features

--- web_crawler/crawler.py
+++ web_crawler/crawler.py
@@ -7,17 +7,14 @@ from requests import Session
 
 from web_crawler.hyperlink import Hyperlink
 from web_crawler.hyperlink import make_hyperlink
+from web_crawler.hyperlink import make_hyperlink_set
 from web_crawler.parser import get_hrefs_from_html
 from web_crawler.requester import ClientError
 from web_crawler.requester import Requester
 from web_crawler.requester import ServerError
 from web_crawler.requester import WrongMIMEType
 
-DEFAULT_TIMEOUT = 10
-DEFAULT_MAX_WORKERS = 1
 DEFAULT_USER_AGENT = "PyWebCrawler"
-DEFAULT_OBEY_ROBOTS = True
-DEFAULT_CHECK_HEAD = False
 
 
 class NoThreadExecutor:
@@ -40,24 +37,25 @@ class Crawler:
         self,
         user_agent: str = DEFAULT_USER_AGENT,
         session: Session = None,
-        max_workers: int = DEFAULT_MAX_WORKERS,
-        timeout: int = DEFAULT_TIMEOUT,
-        obey_robots: bool = DEFAULT_OBEY_ROBOTS,
-        check_head: bool = DEFAULT_CHECK_HEAD,
+        max_workers: int = 1,
+        timeout: int = 10,
+        obey_robots: bool = True,
+        check_head: bool = False,
     ):
+        # config elements
         self.user_agent = user_agent
-        self.requester = Requester(user_agent=user_agent, session=session)
-
-        self.queue = queue.Queue()
-
-        self.seen_urls = set()
-        self.done_urls = set()
-
         self.max_workers = max_workers
         self.timeout = timeout
-
         self.obey_robots = obey_robots
         self.check_head = check_head
+        self.trim_query = True
+        self.trim_fragment = True
+
+        # setup internal elements
+        self._requester = Requester(user_agent=user_agent, session=session)
+        self._queue = queue.Queue()
+        self._seen_urls = make_hyperlink_set()
+        self._done_urls = make_hyperlink_set()
 
     def executor(self):
         """executor for multi-threaded execution or same script execution if workers=1"""
@@ -72,30 +70,33 @@ class Crawler:
         """crawl any url for all the other urls (in <a hrefs=url> tags)"""
         print(f"crawling {url}")
         try:
-            resp = self.requester(url, check_head_first=self.check_head)
+            resp = self._requester(url, check_head_first=self.check_head)
             hrefs = get_hrefs_from_html(resp.text)
             hrefs = (
-                hrefs.without_fragments().join_all(url).filter_by(authority=url.authority).dedupe()
+                hrefs.trim(query=self.trim_query, fragment=self.trim_fragment)
+                .join_all(url)
+                .filter_by(authority=url.authority)
             )
             for href in hrefs:
-                if href not in self.seen_urls:
-                    self.queue.put(href)
-                    self.seen_urls.add(href)
+                if href not in self._seen_urls:
+                    self._queue.put(href)
+                    self._seen_urls.add(href)
+
+            self._done_urls.add(url)
 
         except (ClientError, ServerError) as exc:
             print(f"{exc} on {url}")
 
         except WrongMIMEType as exc:
             print(f"{exc} on {url}")
-
-        self.done_urls.add(url)
+            self._done_urls.add(url)
 
     def get_robots(self, domain: Hyperlink) -> RobotFileParser:
         """get the robots.txt from any domain"""
         robots_url = domain.with_path("/robots.txt")
         robots = RobotFileParser(str(robots_url))
         try:
-            resp = self.requester(robots_url, mime_types=("text/plain",))
+            resp = self._requester(robots_url, mime_types=("text/plain",))
             robots.parse(resp.text.splitlines())
 
         except (ClientError, ServerError, WrongMIMEType):
@@ -106,26 +107,26 @@ class Crawler:
     def crawl(self, domain: str):
         """crawl any site for all urls"""
         domain = make_hyperlink(domain)
-        self.queue.put(domain)
+        self._queue.put(domain)
 
         robots = self.get_robots(domain)
 
         with self.executor() as executor:
             while True:
                 # exit if we have crawled all urls found
-                if self.seen_urls == self.done_urls and self.seen_urls != set():
+                if self._seen_urls == self._done_urls and self._seen_urls.is_not_empty():
                     # return results
                     return self.render_results()
 
                 # wait for more urls to enter queue or return if we timeout
                 try:
-                    url = self.queue.get(timeout=self.timeout)
+                    url = self._queue.get(timeout=self.timeout)
                 except queue.Empty:
                     # return results
                     return self.render_results()
 
                 # if the url has been done start flow again
-                if url in self.done_urls:
+                if url in self._done_urls:
                     continue
 
                 # if we are to obey the robots then we need to see what we can scrape
@@ -144,8 +145,8 @@ class Crawler:
 
     def render_results(self):
         """render all urls as a set of strings and reset crawler"""
-        results = {str(url) for url in self.done_urls}
-        self.queue = queue.Queue()
-        self.seen_urls = set()
-        self.done_urls = set()
+        results = {str(url) for url in self._done_urls}
+        self._queue = queue.Queue()
+        self._seen_urls = make_hyperlink_set()
+        self._done_urls = make_hyperlink_set()
         return results
